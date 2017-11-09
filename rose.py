ecbc4018d20c4be66a7dfb10546757548017fcfc
Roderic Morris <rmorris@drift.com>
2017-11-09 12:34:36 -0500

    Put a tags directly in messages

--- rose.py
+++ rose.py
@@ -10,10 +10,6 @@ ASKED = 'ASKED'
 CONFUSED = 'CONFUSED'
 SATISFIED = 'SATISFIED'
 
-HIGH_CLASS_ROSE_LINK = 'https://drizly.com/meiomi-rose/p61784'
-BRO_ROSE_LINK = 'https://drizly.com/charles-and-charles-rose/p2516'
-BASIC_ROSE_LINK = 'https://drizly.com/white-girl-rose/p20394'
-
 INITIAL_BODY = u'''<p>Well hello there, it sounds like you're thirsty as hell and in need of a refreshing drink. Might I interest you in a ros\xe9?</p>
 <p>I've quite a selection, but how about you tell me a little about yourself so I can refer you to the proper drink?</p>
 <p>Are you currently on a yacht or feel you deserve to be?</p>
@@ -50,13 +46,13 @@ def make_message(orgId, body, type, buttons=[]):
 
 def respond_to_probable_button_response(conversationId, orgId, body):
     if body == BOAT_RESPONSE:
-        post_message(conversationId, make_message(orgId, 'This should satisfy you, bourgeois animal ' + HIGH_CLASS_ROSE_LINK, 'chat'))
+        post_message(conversationId, make_message(orgId, '<a href="https://drizly.com/meiomi-rose/p61784">This</a> should satisfy you, bourgeois animal', 'chat'))
         return SATISFIED
     elif body == BRO_RESPONSE:
-        post_message(conversationId, make_message(orgId, 'BROOOOOOO ' + BRO_ROSE_LINK, 'chat'))
+        post_message(conversationId, make_message(orgId, '<a href="https://drizly.com/charles-and-charles-rose/p2516">BROOOOOOO</a>', 'chat'))
         return SATISFIED
     elif body == BASIC_RESPONSE:
-        post_message(conversationId, make_message(orgId, 'How you go girl ' + BASIC_ROSE_LINK, 'chat'))
+        post_message(conversationId, make_message(orgId, '<a href="https://drizly.com/white-girl-rose/p20394">Here</a> you go girl', 'chat'))
         return SATISFIED
     elif body == NONE_RESPONSE:
         post_message(conversationId, make_message(orgId, 'Ughh.. what? Wait, are you sure?', 'chat', CONFUSED_RESPONSE_BUTTONS))
